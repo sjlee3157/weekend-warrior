d8ca827215897a357022a3598ee68feb35695a6a
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-08-17 09:41:38 -0700

    hello_world method greets user by name. includes default argument.

--- 00-hello-world/hello_world_spec.rb
+++ 00-hello-world/hello_world_spec.rb
@@ -9,14 +9,14 @@ describe "Hello World" do
   end
 
   it "When given 'Alice' it should greet Alice!" do
-    expect(hello_world 'Alice').must_equal 'Hello, Alice!'
+    expect(hello_world name:'Alice').must_equal 'Hello, Alice!'
   end
 
   it "When given 'Bob' it should greet Bob!" do
-    expect(hello_world 'Bob').must_equal 'Hello, Bob!'
+    expect(hello_world name:'Bob').must_equal 'Hello, Bob!'
   end
 
   it "When given an empty string it should greet the world!" do
-    expect(hello_world '').must_equal 'Hello, World!'
+    expect(hello_world name:'').must_equal 'Hello, World!'
   end
 end
