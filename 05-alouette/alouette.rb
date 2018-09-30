6483e7c4109192aab48bc93c4c50a8ea5fdf0627
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-09-29 21:34:17 -0700

    Added self.verse (tests pass)

--- 05-alouette/alouette.rb
+++ 05-alouette/alouette.rb
@@ -1,8 +1,13 @@
-PARTS = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
+require "pry"
 
 class Alouette
 
+  PARTS = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
+  VERSE_END = "Alouette!\nAlouette!\nA-a-a-ah"
+  REFRAIN = "Alouette, gentille alouette,\nAlouette, je te plumerai."
+
   def self.lines_for_verse(verse_num)
+    # lines_for_verse(2) should return ["Et les yeux!", "Et le bec!", "Et la tête!"]
     lines_for_verse = []
     n = verse_num
     while n >= 0
@@ -10,11 +15,24 @@ class Alouette
       lines_for_verse << line
       n -= 1
     end
-
     return lines_for_verse
   end
 
   def self.verse(verse_num)
+    verse = ""
+    2.times do
+      je_te = "Je te plumerai" + " " + PARTS[verse_num] + "." + "\n"
+      verse << je_te
+    end
+
+    lines_for_verse = self.lines_for_verse(verse_num)
+    lines_for_verse.each do |line|
+      2.times do
+        verse << line + "\n"
+      end
+    end
+
+    verse << VERSE_END
   end
 
   def self.sing
--- 05-alouette/alouette_spec.rb
+++ 05-alouette/alouette_spec.rb
@@ -33,12 +33,10 @@ describe Alouette do
 
   describe "verse" do
     it "returns a string" do
-      skip
       Alouette.verse(3).must_be_kind_of String
     end
 
     it "first two lines begin with 'Je te plumerai'" do
-      skip
       lines = Alouette.verse(3).split("\n")
 
       # If there aren't at least 2 lines, don't continue
@@ -50,7 +48,6 @@ describe Alouette do
     end
 
     it "last three lines are 'Alouette! Alouette! A-a-a-ah'" do
-      skip
       lines = Alouette.verse(3).split("\n")
 
       # If there aren't at least 3 lines, don't continue
@@ -62,7 +59,6 @@ describe Alouette do
     end
 
     it "middle lines begin with 'Et ' and end with '!'" do
-      skip
       lines = Alouette.verse(3).split("\n")
 
       # If there aren't at least 6 lines, don't continue
@@ -78,7 +74,6 @@ describe Alouette do
     end
 
     it "generates the third verse" do
-      skip
       expected_verse = <<-__END_VERSE__
       Je te plumerai les yeux.
 Je te plumerai les yeux.
